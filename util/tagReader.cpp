f17527905b42dc3b14b5ae583b949dfc036cd523
Megan Johnson <megan.j45672000@gmail.com>
2025-05-07 22:20:55 -0400

    worked on tag reader

--- util/tagReader.cpp
+++ util/tagReader.cpp
@@ -1,18 +1,48 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
 
 std::fstream tagList;
 std::string tagListName;
+std::string tagListLine;
+std::vector<std::string> tagListVector;
+std::string getTag();
+void loadTags();
 
-int main(int argc, char** argv){
-    tagListName = *argv[1];
-    tagList.open(tagListName);
+void printVec();
+
+int main(int argc, char** argv){;
+    tagList.open(argv[1]);
+    //getTag();
+    //std::cout << tagListLine << "\n";
+    loadTags();
+    printVec();
+    return 0;
 }
 
 std::string getTag(){
-    //TODO: research getline, I cannot parse that documentation.
-    //tagList.getline(); 
+    if(tagList){
+         std::getline(tagList, tagListLine);
+    }
+    else{
+        std::cout << "File not open\n" << tagListName << "\n";
+    }
 
-    return "";
+    return tagListLine;
+}
+
+void loadTags(){
+    int i = 0;
+    while(!EOF){
+        std::getline(tagList, tagListLine);
+        tagListVector[i] = tagListLine;
+    }
+}
+
+void printVec(){
+    std::cout << "boop\n";
+    for(unsigned long i = 0; i < tagListVector.size(); i++){
+        std::cout << tagListVector[i] << "\n";
+    }
 }
