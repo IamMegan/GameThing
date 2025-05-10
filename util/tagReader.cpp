44fe11187a7ea41692693a4ec479d7baafe23500
Megan Johnson <megan.j45672000@gmail.com>
2025-05-09 21:18:19 -0400

    improved tagReader

--- util/tagReader.cpp
+++ util/tagReader.cpp
@@ -4,26 +4,29 @@
 #include <vector>
 
 std::fstream tagList;
-std::string tagListName;
-std::string tagListLine;
+std::string tagListName = "";
+std::string tagListLine = "";
 std::vector<std::string> tagListVector;
+
 std::string getTag();
 void loadTags();
-
 void printVec();
+void pruneTags();
 
 int main(int argc, char** argv){;
     tagList.open(argv[1]);
-    //getTag();
-    //std::cout << tagListLine << "\n";
-    loadTags();
+    getTag();
+    printVec();
+    pruneTags();
     printVec();
     return 0;
 }
 
+//Going to be reworked, don´t touch for now. 
 std::string getTag(){
     if(tagList){
-         std::getline(tagList, tagListLine);
+        std::cout << "File opened succesfully!\n";
+        loadTags();
     }
     else{
         std::cout << "File not open\n" << tagListName << "\n";
@@ -32,17 +35,36 @@ std::string getTag(){
     return tagListLine;
 }
 
+//Load tags into active memory to preform operations on.
 void loadTags(){
     int i = 0;
-    while(!EOF){
-        std::getline(tagList, tagListLine);
-        tagListVector[i] = tagListLine;
-    }
+    while(true){
+        if(tagList.eof()){
+            std::cout << "TagList EOF!\n";
+            break;
+        }
+            tagListVector.resize(i+1);
+            std::getline(tagList, tagListLine);
+            tagListVector[i] = tagListLine;
+            i++;
+        }
+    
+        if(tagListVector.size() == 0){
+            std::cout << "Tag List Vector is empty\n";
+        }
 }
 
 void printVec(){
-    std::cout << "boop\n";
     for(unsigned long i = 0; i < tagListVector.size(); i++){
         std::cout << tagListVector[i] << "\n";
+    }
+}
+
+void pruneTags(){
+    for(unsigned long i = 0; i < tagListVector.size(); i++){
+        if(!tagListVector[i].compare("")){
+           tagListVector.erase(tagListVector.begin() + i);
+           i--;
+        }
     }
 }
