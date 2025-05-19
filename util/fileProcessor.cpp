3d7f317ce1f0d15bff8bc89ad94a6456ebb995ee
Megan Johnson <eartmeisterindigo@gmail.com>
2025-05-19 00:06:07 -0400

    surprisingly painless process of getting the file type into a string object.

--- util/fileProcessor.cpp
+++ util/fileProcessor.cpp
@@ -4,35 +4,59 @@
 #include <vector>
 #include <sstream>
 
-enum fileType {$material};
+enum fileType {$material, $species};
 std::fstream file;
 std::string fileType;
 std::stringstream stream;
 std::vector<std::string> contents;
 void loadFile();
+std::vector<std::string> packageData();
 
 int main(int argc, char** argv){;
     file.open(argv[1]);
     if(file){
       std::cout << "File open!" << "\n";
       loadFile();
+      packageData();
+      return 1;
     }
     else{
       std::cout << "File not opened." << "\n";
+      return 1;
     }
-    return 0;
 }
 
-//Load file contents into the stream
+//Load file contents into the stream. Note this does not populate a vector or anything, simply the stringstream object.
 void loadFile(){
     std::cout << "loading file..." << "\n";
-    //avoid the problem entirely by passing the stream pointer from the fstream to the sstream. No loop needed! I think that's whats happening
+    //avoid the problem entirely by passing the stream pointer from the fstream to the sstream. No loop needed! I think that's whats happening. Not actually sure what's happening within the library.
     stream << file.rdbuf();
     file.close();
     if(stream.str().length() == 0){
       std::cout << "Reading failed" << "\n";
     }
     else{
-      std::cout << "read success!" << "\n" << stream.str() << "\n";
+      std::cerr << "read success!" << "\n" << stream.str() << "\n";
     }
+}
+
+std::vector<std::string> packageData(){
+  //Now that I'm understanding stringstream better, this is so nice. 
+  stream >> fileType;
+  
+  if(!fileType.compare("$material")){
+    std::cout << "This is a material file" << "\n";
+
+  }
+  else if (!fileType.compare("$species")) {
+    
+  }
+  else{
+    std::cerr << "Unrecognized fileType." << "\n";
+  }
+    
+  //testing stuff
+  std::vector<std::string> temptestvec;
+  temptestvec.push_back(fileType);
+  return temptestvec;
 }
